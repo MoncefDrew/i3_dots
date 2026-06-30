944900595266720b5788409317548a5039069968
MoncefDrew <moncef.mokrani@etu.univ-batna2.dz>
2026-07-01 00:10:59 +0100

    add:some configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
 end
+
+alias aseprite "/home/moncef/Documents/deps/aseprite/build/bin/aseprite" 
+alias os 'cd ~/Documents/books/pending/OS/'
+alias np 'cd ~/Documents/books/pending/NetworkProg/'
+alias c 'cd ~/Documents/books/pending/Cprogramming/'
+alias lnx "cd ~/Documents/books/pending/linux"
+alias yw7 "mono ~/.wine32/drive_c/Program\ Files\ \(x86\)/yWriter7/yWriter7.exe"
+
+
